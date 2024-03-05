6480c0e8e2a95ec9db6e73c8ae01c297aeccf011
Vitalik Buterin <VitalikZhou@gmail.com>
2024-03-05 19:43:09 +0800

    name

--- dag.go
+++ dag.go
@@ -2,6 +2,17 @@ package merkledag
 
 import "hash"
 
+type IPFSLink struct {
+	Name string
+	Hash []byte
+	Size int
+}
+
+type Object struct {
+	Links []IPFSLink
+	Data  []byte
+}
+
 func Add(store KVStore, node Node, h hash.Hash) []byte {
 	// TODO 将分片写入到KVStore中，并返回Merkle Root
 	return nil
